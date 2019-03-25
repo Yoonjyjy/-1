2c1dbd1af8650ec1589fa25e0441235cd73bf508
Yoonjyjy <41252790+Yoonjyjy@users.noreply.github.com>
2019-03-26 02:20:06 +0900

    computerNetwork_week3
    주석이 파일이 올라가는 과정에서 자동으로 사라져 다시 추가

--- server2.py
+++ server2.py
@@ -13,25 +13,25 @@ def run_server(port=4000):
         s.listen(1) ## max 1 client
 
         conn, addr = s.accept()
-        msg = conn.recv(1024)
-        print("file name : " + msg.decode())
+        msg = conn.recv(1024)   ## 파일 이름을 받아옴(*전달내용 1*)
+        print("file name : " + msg.decode()) ## 'file name : (파일 이름)'으로 출력
         
-        if os.path.isfile(sys.argv[4]+'/'+msg.decode()):
-                conn.sendall("OK".encode())
-                file_size = os.path.getsize(sys.argv[4]+'/'+msg.decode())
-                print("size : "+str(file_size))
-                conn.sendall(str(file_size).encode())
-                resp = conn.recv(2)
+        if os.path.isfile(sys.argv[4]+'/'+msg.decode()): ## 지정한 경로에 원하는 파일이 있는지 검사
+                conn.sendall("OK".encode())     ## *전달내용 2-1*, 파일이 있을 경우 OK 사인을 보내줌 
+                file_size = os.path.getsize(sys.argv[4]+'/'+msg.decode()) ## 파일의 크기를 구해 저장
+                print("size : "+str(file_size)) ## 'size : (파일 크기)'로 출력
+                conn.sendall(str(file_size).encode()) ## *전달내용 3*, 파일의 크기를 전송
+                resp = conn.recv(2) ## *전달내용 4*, OK 사인을 받음
 
-                filename = sys.argv[4]+'/'+msg.decode()
-                file = open(filename, 'rb')
-                text_str = file.read(file_size)
-                ## print(text_str)
-                file.close()
+                filename = sys.argv[4]+'/'+msg.decode() ## 파일을 읽기 위해 filename에 파일의 주소와 파일 이름을 합해 저장
+                file = open(filename, 'rb') ## 파일을 읽음
+                text_str = file.read(file_size) ## text_str에 아까 구한 파일의 사이즈만큼 파일을 읽어 저장
+                file.close() ## 파일 닫기
 
-                conn.sendall(text_str)
-        else :
-                conn.sendall("-1".encode())
+                conn.sendall(text_str) ## *전달내용 5*, 저장한 파일의 내용들을 전송
+
+        else :                                  ## 지정했던 경로에 원하는 파일이 없을 경우 -1을 보내줌
+                conn.sendall("-1".encode())     ## *전달내용 2-2*
 
         conn.sendall(msg)
         conn.close()
