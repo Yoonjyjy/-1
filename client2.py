bb8386babdc191a35a3d67d26c72ab861e3b24f6
Yoonjyjy <41252790+Yoonjyjy@users.noreply.github.com>
2019-03-26 02:20:55 +0900

    computerNetwork_week3
    주석이 올리는 과정에서 자동으로 삭제되어 주석 추가

--- client2.py
+++ client2.py
@@ -8,21 +8,20 @@ def run(host, port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
         line = input("file name : ")
-        s.sendall(line.encode())
-        sign = s.recv(2)
+        s.sendall(line.encode())    ## *전달내용 1*
+        sign = s.recv(2)            ## *전달내용 2*, 경로에 파일이 있는지 검사한 결과로 OK나 -1을 받음
 
-        if str(sign.decode()) != "-1" :
-            size = s.recv(1024)
+        if str(sign.decode()) != "-1" : ## 경로에 파일이 없는 경우가 아니면(=있으면) 실행, 파일이 없으면 종료
+            size = s.recv(1024) ## *전달내용 3*, 파일의 크기를 전달받음
             size = size.decode()
-            print("size : " + size)
-            s.sendall('OK'.encode())
+            print("size : " + size) ## 'size : (파일 크기)'로 출력해줌
+            s.sendall('OK'.encode()) ## *전달내용 4*, 사이즈를 전달받고 OK 전송
 
-            txt_str = s.recv(int(size))
-            ##print(str(txt_str.decode()))
+            txt_str = s.recv(int(size)) ## *전달내용 5*, 아까 전달받은 사이즈만큼만 txt_str에 전달받음
 
-            file = open(line, 'wb')
-            file.write(txt_str)
-            file.close()
+            file = open(line, 'wb') ## line(맨 처음 입력받은 파일 이름)을 이용해 똑같은 이름의 파일 쓰기
+            file.write(txt_str) ## 그 안에 txt_str(전달받은 파일 내용물)을 작성
+            file.close() ## 파일 닫기
 
 
 if __name__ == '__main__':
